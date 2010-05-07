7baf07a9c17335ec1c2f62f688d4841d4066fcbf
"Lorenzo M. Catucci" <lorenzo@sancho.ccd.uniroma2.it>
2010-05-08 01:40:07 +0200

    [mq]: 3_identity_in_ticket.diff

--- repoze/who/plugins/use_beaker.py
+++ repoze/who/plugins/use_beaker.py
@@ -10,26 +10,34 @@ from zope.interface import implements
 
 from repoze.who.interfaces import IIdentifier
 
+PERSIST_KEYS = ['userdata']
 
 class UseBeakerPlugin(object):
 
     implements(IIdentifier)
     
-    def __init__(self,
-            key_name='repoze.who.tkt',
-            session_name='beaker.session',
-            delete_on_logout=False):
+    def __init__(self, key_name='repoze.who.tkt',
+                       session_name='beaker.session', delete_on_logout=False,
+                       alsopersist = PERSIST_KEYS):
+        """Create an identification plugin storing at least the ``identity['repoze.who.userid']`` item
+        in a beaker session.
+
+        :param alsopersist: names of additional identity items saved in session. Default: ``alsopersist==['userdata']``
+        :type alsopersist: sequence
+        """
+        
         self.key_name = key_name
         self.session_name = session_name
         self.delete_on_logout = delete_on_logout
+        self.persistkeys = alsopersist
 
     def identify(self, environ):
         """Return identity from Beaker session"""
 
         s = self._get_beaker(environ)
-        userid = s.get(self.key_name, None)
-        if userid:
-            return {'repoze.who.userid': userid}
+        identity = s.get(self.key_name, None)
+        if identity and identity.get('repoze.who.userid'):
+            return identity
 
     def forget(self, environ, identity):
         """Does not return any headers, just deletes the session entry.
@@ -58,11 +66,20 @@ class UseBeakerPlugin(object):
         
         """
         s = self._get_beaker(environ)
-        userid = s.get(self.key_name)
+        pidentity = s.get(self.key_name)
+        if pidentity:
+            puserid = pidentity.get('repoze.who.userid')
+        else:
+            puserid = None
 
         iuserid = identity.get('repoze.who.userid')
-        if userid != iuserid:
-            s[self.key_name] = iuserid
+        if puserid != iuserid:
+            tkt_identity = {'repoze.who.userid': iuserid}
+            for i in self.persistkeys:
+                item = identity.get(i)
+                if item:
+                    tkt_identity[i] = item
+            s[self.key_name] = tkt_identity
             s.save()
         return []
 
@@ -79,5 +96,6 @@ class UseBeakerPlugin(object):
     def __repr__(self):
         return '<%s %s>' % (self.__class__.__name__, id(self))
 
-def make_plugin(key_name='repoze.who.tkt', session_name='beaker.session'):
-    return UseBeakerPlugin(key_name, session_name)
+def make_plugin(key_name='repoze.who.tkt', session_name='beaker.session', delete_on_logout=False, alsopersist = PERSIST_KEYS):
+    """see :class:`UseBeakerPlugin`"""
+    return UseBeakerPlugin(key_name, session_name, delete_on_logout, alsopersist)
